b086d3f5895c580bcde6e962d518c8752d252030
Nick-Singstock <35930611+Nick-Singstock@users.noreply.github.com>
2021-10-11 08:50:49 -0600

    Update jdft_helper.py

--- manager/jdft_helper.py
+++ manager/jdft_helper.py
@@ -421,7 +421,7 @@ class helper():
         return None #data_analysis(data)
     
 #    @property
-    def reference_molecules():
+    def reference_molecules(self):
         refs = {'H': {'refs': ['H2'], 'coeffs': [0.5]},
                 'H2': {'refs': ['H2'], 'coeffs': [1]},
                 'H2O': {'refs': ['H2O'], 'coeffs': [1]},
